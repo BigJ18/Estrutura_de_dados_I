6004f6f7762b2b3306b4120bffa6fbca92a9d923
BigJ18 <130506584+BigJ18@users.noreply.github.com>
2023-10-31 15:28:42 -0400

    Create ponto.h

---
+++ Atividades/2023102600/ponto.h
@@ -0,0 +1,15 @@
+#ifndef PONTO_H_INCLUDED
+#define PONTO_H_INCLUDED
+#endif
+
+typedef struct ponto Ponto;
+
+Ponto *Cria_ponto(float x, float y);
+
+void Libera_ponto (Ponto *pto);
+
+int Acessa_ponto (Ponto *pto, float *x, float *y);
+
+int Atribuir_ponto(Ponto *pto, float x, float y);
+
+float Distancia_ponto(Ponto *pto1, Ponto *pto2);
